34a5ae092305d69fa44dbe4b6b2b6f980b4d361d
Darc99 <lennyyhugoh@gmail.com>
2022-06-22 00:55:55 +0100

    tests and fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+
 """Write a class Square that defines a square by: (based on 0-square.py)"""
 
+
 class Square:
-    def __init__(self, size) -> None:
-        """
-        Private instance attribute: size
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
         """
         self.__size = size
