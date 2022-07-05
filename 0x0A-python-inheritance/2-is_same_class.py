572e51acf74e1818108ff2b19d73d9c989f73fc5
Darc99 <lennyyhugoh@gmail.com>
2022-07-05 02:28:33 +0100

    2-5

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -6,4 +6,4 @@ Contains the function is_same_class
 
 def is_same_class(obj, a_class):
     """return True if object is exactly an instance of the specified class ; otherwise False"""
-    return (type(obj) == a_class)
+    return True if type(obj) == a_class else False
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,4 +6,7 @@ Contains the inherits_from function (Only sub class of )
 
 def inherits_from(obj, a_class):
     """returns True if the object is an instance of a class that inherited (directly or indirectly) from the specified class ; otherwise False."""
-    return(issubclass(type(obj), a_class) and type(obj) != a_class)
+    if type(obj) != a_class:
+        return issubclass(type(obj), a_class)
+    else:
+        return False
