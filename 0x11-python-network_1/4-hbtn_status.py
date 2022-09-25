ffdc61e436e05e4ef7fa25ee3a3a45d69c2c890d
Darc99 <lennyyhugoh@gmail.com>
2022-09-25 13:20:22 +0100

    task fix

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""This script fetches https://intranet.hbtn.io/status."""
-
+"""script fetches https://intranet.hbtn.io/status."""
 import requests
+
 
 if __name__ == "__main__":
     r = requests.get("https://intranet.hbtn.io/status")
